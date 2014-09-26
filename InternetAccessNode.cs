665cc03814a11c90ae6b552de57a8a9c54c851b2
jens <jens@lea.io>
2014-09-26 19:37:24 +0200

    Using a service subcrition for super low no thread notification

--- InternetAccessNode.cs
+++ InternetAccessNode.cs
@@ -15,121 +15,130 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Linq;
 
-using System.Threading;
-using System.ComponentModel;
-
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 
+using System.Threading;
+
+using NETWORKLIST;
 
 #endregion usings
 
 namespace VVVV.Nodes
 {
 	#region PluginInfo
-	[PluginInfo(Name = "InternetAccess", Category = "Network", Version = "Value", Help = "Checks if any internet connection is available", Tags = "")]
+	[PluginInfo(Name = "InternetAccess", Category = "Network", Version = "Value", Author = "jens", Help = "Checks if any internet connection is available", Tags = "")]
 	#endregion PluginInfo
-    public class InternetAccessNode : IPluginEvaluate, IPartImportsSatisfiedNotification, IDisposable
+    public class InternetAccessNode : IPluginEvaluate, IPartImportsSatisfiedNotification, IDisposable, INetworkListManagerEvents
 	{
 		#region fields & pins
-		[Input("Interval", IsSingle = true, MinValue = 1, DefaultValue = 1)]
-		IDiffSpread<int> IInterval;
-		
-		[Output("Internet Connection")]
-		public ISpread<int> InternetConnections;
-		[Output("Last Time Checked")]
+		[Output("Internet Connection", IsToggle=true)]
+		public ISpread<bool> InternetConnections;
+
+        [Output("Available Networks")]
+        public ISpread<string> AvailableNetworks;
+
+		[Output("Last Time Networks Changed")]
 		public ISpread<string> CheckenOn;
 		
 		[Import()]
 		public ILogger Logger;
 		#endregion fields & pins
-		
-		
+
+        private int m_cookie = 0;
+        private IConnectionPoint m_icp;
+        private INetworkListManager m_nlm;
+        private bool HasInternet = false;
+        private DateTime LastCheck = DateTime.MinValue;
+
 		#region pin management
 		public void OnImportsSatisfied()
 		{
-			IInterval.Changed += delegate(IDiffSpread<int> Intervals) {
-				lock (IntervalLock) {
-					Interval = Intervals[0];
-				}
-			};
-			StartConnectionMonitor();
+            AvailableNetworks.SliceCount = 0;
+            Networks = NLM.GetNetworks(NLM_ENUM_NETWORK.NLM_ENUM_NETWORK_CONNECTED);
+            LastCheck = DateTime.Now;
+            HasInternet = NLM.IsConnectedToInternet;
+
+            AdviseforNetworklistManager();
+            InternetConnections.SliceCount = 1;
+            CheckenOn.SliceCount = 1;
 		}
+
 		public InternetAccessNode():base() {
-			ID++;
+            m_nlm = new NetworkListManager();
+            Networks = NLM.GetNetworks(NLM_ENUM_NETWORK.NLM_ENUM_NETWORK_CONNECTED);
 		}
-		// Do it in a BackgroundWorker and shrare it among the nodes....
-		static BackgroundWorker Worker = new BackgroundWorker();
-		static int ID = 0;
-		static Mutex IntervalLock = new Mutex();
-		static int Interval = 1;
-		void StartConnectionMonitor() {
-			InternetConnections.SliceCount = 1;
-			CheckenOn.SliceCount = 1;
-			Worker.RunWorkerCompleted += delegate(object sender, RunWorkerCompletedEventArgs args) {
-				if (args.Cancelled) Worker.Dispose();
-			};
-			Worker.DoWork += delegate (object sender, DoWorkEventArgs args) {
-				try {
-					BackgroundWorker w = sender as BackgroundWorker;
-					while(!w.CancellationPending) {
-						w.ReportProgress(IsAvailableNetworkActive() ? 1 : 0);
-						
-						int _Interval = 1;
-						lock(IntervalLock) {
-							_Interval = Interval;
-						}
-						for	(int i=0; i < 100 * Interval; i++) {
-							Thread.Sleep(10);
-							args.Cancel = w.CancellationPending;
-							if(args.Cancel) return;
-						}
-					}
-					
-				} catch( Exception e) {}
-			};
-			
-			Worker.ProgressChanged += delegate(object sender, ProgressChangedEventArgs args) {
-				try {
-					InternetConnections[0] = args.ProgressPercentage;
-					CheckenOn[0] = DateTime.Now.ToLocalTime().ToString();
-				} catch (Exception e) {}
-			};
-			
-			if (Worker.IsBusy) return;
-			Worker.WorkerReportsProgress = true;
-			Worker.WorkerSupportsCancellation = true;
-			Worker.RunWorkerAsync();
-		}
-		
+
+        IEnumNetworks Networks;
+
+        public INetworkListManager NLM
+        {
+            get
+            {
+                return m_nlm;
+            }
+        }
+
+
+        public void ConnectivityChanged(NLM_CONNECTIVITY newConnectivity)
+        {
+            //Console.WriteLine("");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("NetworkList just informed the connectivity change. The new connectivity is:");
+            if (newConnectivity == NLM_CONNECTIVITY.NLM_CONNECTIVITY_DISCONNECTED)
+            {
+               // Console.WriteLine("The machine is disconnected from Network");
+                HasInternet = false;
+            }
+            if (((int)newConnectivity & (int)NLM_CONNECTIVITY.NLM_CONNECTIVITY_IPV4_INTERNET) != 0)
+            {
+                //Console.WriteLine("The machine is connected to internet with IPv4 capability ");
+                HasInternet = true;
+            }
+            if (((int)newConnectivity & (int)NLM_CONNECTIVITY.NLM_CONNECTIVITY_IPV6_INTERNET) != 0)
+            {
+                //Console.WriteLine("The machine is connected to internet with IPv6 capability ");
+                HasInternet = true;
+            }
+            if ((((int)newConnectivity & (int)NLM_CONNECTIVITY.NLM_CONNECTIVITY_IPV4_INTERNET) == 0)
+                && (((int)newConnectivity & (int)NLM_CONNECTIVITY.NLM_CONNECTIVITY_IPV6_INTERNET) == 0))
+            {
+                //Console.WriteLine("The machine is not connected to internet yet ");
+                HasInternet = false;
+            }
+            Networks = NLM.GetNetworks(NLM_ENUM_NETWORK.NLM_ENUM_NETWORK_CONNECTED);
+            LastCheck = DateTime.Now;
+        }
+        public void AdviseforNetworklistManager()
+        {
+            IConnectionPointContainer icpc = (IConnectionPointContainer)m_nlm;
+            Guid tempGuid = typeof(INetworkListManagerEvents).GUID;
+            icpc.FindConnectionPoint(ref tempGuid, out m_icp);
+            m_icp.Advise(this, out m_cookie);
+        }
+
+        public void UnAdviseforNetworklistManager()
+        {
+            m_icp.Unadvise(m_cookie);
+        }        
+
 		#endregion
 		
 		public void Dispose() {
-			ID--;
-			if (ID < 1) Worker.CancelAsync();
+            UnAdviseforNetworklistManager();
 		}
 		
 		// Called when data for any output pin is requested.
 		public void Evaluate(int SpreadMax)
 		{
+            InternetConnections[0] = HasInternet;
+            CheckenOn[0] = LastCheck.ToString();
+            try
+            {
+                AvailableNetworks.AssignFrom(from INetwork net in Networks where net.IsConnectedToInternet select net.GetName());
+            }
+            catch (Exception e) { }
 		}
 		
-		
-		// AS OF: http://stackoverflow.com/a/24473982
-		public static bool IsAvailableNetworkActive()
-		{
-			// only recognizes changes related to Internet adapters
-			if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
-			{
-				// however, this will include all adapters -- filter by opstatus and activity
-				NetworkInterface[] interfaces = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
-				return (from face in interfaces
-				where face.OperationalStatus == OperationalStatus.Up
-				where (face.NetworkInterfaceType != NetworkInterfaceType.Tunnel) && (face.NetworkInterfaceType != NetworkInterfaceType.Loopback)
-				select face.GetIPv4Statistics()).Any(statistics => (statistics.BytesReceived > 0) && (statistics.BytesSent > 0));
-			}
-			
-			return false;
-		}
 	}
 }
